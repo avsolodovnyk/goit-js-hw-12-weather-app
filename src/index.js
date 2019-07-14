0ba3e39ce311b53dfbbb0dc8b45201cb96a6cd82
Антон Солодовник <av.solodovnyk@gmail.com>
2019-07-14 21:25:22 +0300

    hw 13 rev.1.01'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+'use strict';
 import './styles.css';
 import '../node_modules/pnotify/dist/PNotifyBrightTheme.css';
 import './scripts/mainModel';
--- src/scripts/fetchWeather.js
+++ src/scripts/fetchWeather.js
@@ -2,7 +2,7 @@
 export default function fetchWeather(query) {
   return new Promise((resolve, reject) => {
     const currentWeather = fetch(
-      `http://api.apixu.com/v1/current.json?key=a111d758e3d74685acc181313191107&q=${query}&lang=ru`,
+      `https://api.apixu.com/v1/current.json?key=a111d758e3d74685acc181313191107&q=${query}&lang=ru`,
     )
       .then(response => {
         if (response.ok) {
